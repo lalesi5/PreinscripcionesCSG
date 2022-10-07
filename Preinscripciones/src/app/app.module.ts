880ad826b88201185ca93d7fde97ee5477b543e8
MFerCC <fc36350@gmail.com>
2022-10-07 15:14:48 -0500

    acceso interfaz

--- Preinscripciones/src/app/app.module.ts
+++ Preinscripciones/src/app/app.module.ts
@@ -6,21 +6,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccesoComponent } from './Acceso/acceso/acceso.component';
 import { LoginComponent } from './Login/login/login.component';
+import { AccesoService } from './Servicios/acceso.service';
+import { GeneralComponent } from './Core/General/general/general.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AccesoComponent,
-    LoginComponent
+    LoginComponent,
+    GeneralComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   exports: [RouterModule],
-  providers: [],
+  providers: [
+    AccesoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
