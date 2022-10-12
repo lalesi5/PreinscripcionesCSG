187eb5fa1e2a6f5bce02dbc5ec643e108b84b7c9
MFerCC <fc36350@gmail.com>
2022-10-12 14:12:21 -0500

    prueba

--- Preinscripciones/src/app/app.module.ts
+++ Preinscripciones/src/app/app.module.ts
@@ -4,58 +4,44 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccesoComponent } from './Acceso/acceso/acceso.component';
-import { LoginComponent } from './Login/login/login.component';
 import { AccesoService } from './Servicios/acceso.service';
 import { GeneralComponent } from './Core/General/general/general.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 
-//MODULOS
-import { AppRoutingModule } from "src/app/app-routing.module";
-
-//COMPONENTES
-import { AppComponent } from "src/app/app.component";
-;
 
 @NgModule({
 
   declarations: [
     AppComponent,
-    AccesoComponent,
-    LoginComponent,
     GeneralComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    AccesoRoutingModule,
+    
   ],
   exports: [RouterModule],
   providers: [
     AccesoService
   ],
   bootstrap: [AppComponent]
-
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+ 
 
 })
 export class AppModule{}
