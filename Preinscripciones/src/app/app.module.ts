e69ec0943b64773fdaab2fa8ce9c5fb1ad8ecb63
MFerCC <fc36350@gmail.com>
2022-10-12 10:32:12 -0500

    acceso

--- Preinscripciones/src/app/app.module.ts
+++ Preinscripciones/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccesoComponent } from './Acceso/acceso/acceso.component';
 import { LoginComponent } from './Login/login/login.component';
+<<<<<<< HEAD
+=======
 import { AccesoService } from './Servicios/acceso.service';
 import { GeneralComponent } from './Core/General/general/general.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,30 +16,22 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+>>>>>>> parent of d363e6e... Merge branch 'develop-Mafer' of https://github.com/lalesi5/PreinscripcionesCSG into develop-Mafer
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AccesoComponent,
-    LoginComponent,
-    GeneralComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule
+    
   ],
   exports: [RouterModule],
-  providers: [
-    AccesoService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
