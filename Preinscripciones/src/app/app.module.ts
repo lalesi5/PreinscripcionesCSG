596f41a98a3eeebeda3e1ca1a0dfb2d1ddd49e64
JeremyTorres07 <jeremy.torres@epn.edu.ec>
2022-10-12 15:08:05 -0500

    fer2.0

--- Preinscripciones/src/app/app.module.ts
+++ Preinscripciones/src/app/app.module.ts
@@ -6,17 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccesoComponent } from './Acceso/acceso/acceso.component';
 import { LoginComponent } from './Login/login/login.component';
-<<<<<<< HEAD
-=======
-import { AccesoService } from './Servicios/acceso.service';
-import { GeneralComponent } from './Core/General/general/general.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
->>>>>>> parent of d363e6e... Merge branch 'develop-Mafer' of https://github.com/lalesi5/PreinscripcionesCSG into develop-Mafer
+
 
 
 @NgModule({
