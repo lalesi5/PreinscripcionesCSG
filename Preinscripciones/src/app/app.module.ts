0724672353c7ecf3d6c6815f601ef54c6477c978
MFerCC <fc36350@gmail.com>
2022-10-12 15:31:05 -0500

    cambios final

--- Preinscripciones/src/app/app.module.ts
+++ Preinscripciones/src/app/app.module.ts
@@ -4,22 +4,39 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
 
   declarations: [
     AppComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
     
   ],
   exports: [RouterModule],
-  providers: [],
+  providers: [
+  
+  ],
   bootstrap: [AppComponent]
  
 
