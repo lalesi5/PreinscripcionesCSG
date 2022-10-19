350281556ff5f4456346c5d3fbf682852c016ca7
JeremyTorres07 <jeremy.torres@epn.edu.ec>
2022-10-19 10:03:24 -0500

    Implementacion de Errores

--- Preinscripciones/src/app/Login/Sistema/Estudiante/estudiante.component.ts
+++ Preinscripciones/src/app/Login/Sistema/Estudiante/estudiante.component.ts
@@ -14,34 +14,145 @@ interface Food {
 export class EstudianteComponent implements OnInit {
   resultado!: string;
 
+  //RADIO BUTTON
+
+
   //ERROR DE APELLIDOS
-  lastName = new FormControl({
-    lastName: new FormControl('', [Validators.required, Validators.minLength(100)]),});
+  //lastName = new FormControl({
+    lastNameStudent = new FormControl('', [Validators.required]);
 
 
   //ERROR DE NOMBRE
-  name = new FormControl({
-    name: new FormControl('', [Validators.required, Validators.minLength(100)]),});
+  //name = new FormControl({
+    nameStudent = new FormControl('', Validators.required)
 
   /*ERROR DE EMAIL
   email = new FormControl('', [Validators.required, Validators.email]);*/
+
+  //ERROR FECHA
+
+  dateStudent = new FormControl('', Validators.required)
+
+//ERROR CON QUINES VIVE
+
+  homeStudent = new FormControl('', Validators.required)
+
+//ERROR CON CI
+
+  studentCI = new FormControl('', Validators.required)
+
+//ERROR NIVEL AL QUE APLICA
+
+  studentLevel = new FormControl('', Validators.required)
+
+
+//ERROR INSTITUCION ANTERIOR
+
+  studentInstitution = new FormControl('', Validators.required)
+
+
+  //ERROR DIRECCION ESTUDIANTE
+
+  studentAddress = new FormControl ('', Validators.required)
+
+//ERROR  CELULAR1 ESTUDIANTE
+  studentPhone1 = new FormControl ('', Validators.required)
+  studentPhone2 = new FormControl ('', Validators.required)
+
+//ERROR SUBIR CEDULA PDF
+
+  studentCIPDF = new FormControl ('', Validators.required)
+  studentPDF = new FormControl ('', Validators.required)
+
+
+
+
 
 
 
   getErrorMessage() {
 
     //MENSAJE ERROR APELLIDOS
-    if (this.lastName.hasError('required')) {
+    if (this.lastNameStudent.hasError('required')) {
       return 'No dejar campos vacios';
     }
-    return this.lastName.hasError('required') ? 'Espacio Vacio' : '';
+    return this.lastNameStudent.hasError('lastNameStudent') ? 'Espacio Vacio' : '';
 
 
     //MENSAJE ERROR NOMRBES
-    if (this.name.hasError('required')) {
+    if (this.nameStudent.hasError('required')) {
       return 'No dejar campos vacios';
     }
-    return this.name.hasError('required') ? 'Espacio Vacio' : '';
+    return this.nameStudent.hasError('nameStudent') ? 'Espacio Vacio' : '';
+
+  //MENSAJE ERROR FECHA
+    if (this.dateStudent.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.dateStudent.hasError('dateStudent') ? 'Espacio Vacio' : '';
+
+    //MENSAJE ERROR CON QUIEN VIVES
+    if (this.homeStudent.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.homeStudent.hasError('homeStudent') ? 'Espacio Vacio' : '';
+
+  //MENSAJE ERROR CON CI
+    if (this.studentCI.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.studentCI.hasError('studentCI') ? 'Espacio Vacio' : '';
+
+    //MENSAJE ERROR NIVEL AL QUE APLICA
+    if (this.studentInstitution.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.studentInstitution.hasError('studentInstitution') ? 'Espacio Vacio' : '';
+
+
+  //MENSAJE ERROR INSTITUCION ANTERIOR
+    if (this.studentLevel.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.studentLevel.hasError('studentLevel') ? 'Espacio Vacio' : '';
+
+
+    //MENSAJE ERROR INSTITUCION ANTERIOR
+    if (this.studentAddress.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.studentAddress.hasError('studentAddress') ? 'Espacio Vacio' : '';
+
+    //MENSAJE ERROR CELULAR1
+    if (this.studentPhone1.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.studentPhone1.hasError('studentPhone1') ? 'Espacio Vacio' : '';
+
+    //MENSAJE ERROR CELULAR2
+    if (this.studentPhone2.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.studentPhone2.hasError('studentPhone2') ? 'Espacio Vacio' : '';
+
+
+    //MENSAJE ERROR PDF CEDULA
+    if (this.studentCIPDF.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.studentCIPDF.hasError('studentCIPDF') ? 'Espacio Vacio' : '';
+
+    //MENSAJE ERROR LIBRETA
+    if (this.studentPDF.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.studentPDF.hasError('studentPDF') ? 'Espacio Vacio' : '';
+
+
+
+
+
+
 
 
     //MENSAJE ERROR EMAIL
--- Preinscripciones/src/app/Login/Sistema/Representante/representante.component.ts
+++ Preinscripciones/src/app/Login/Sistema/Representante/representante.component.ts
@@ -12,15 +12,137 @@ interface Food {
   styleUrls: ['./representante.component.css']
 })
 export class RepresentanteComponent implements OnInit {
-  email = new FormControl('', [Validators.required, Validators.email]);
+
+  //ERROR DE APELLIDOS
+  lastNameParent = new FormControl('', [Validators.required]);
+
+
+  //ERROR DE NOMBRES
+  //name = new FormControl({
+  nameParent = new FormControl('', Validators.required)
+
+
+  //ERROR DE EMAIL
+  email = new FormControl('', [Validators.required]);
+
+//ERROR CEDULA
+
+  parentCI = new FormControl('', [Validators.required]);
+
+
+//ERROR PARENTEZCO
+  familiar = new FormControl('', [Validators.required]);
+
+
+
+//ERROR DIRECCION REPRESENTANTE
+
+  addressParent = new FormControl('', [Validators.required]);
+
+
+//ERROR TELEFONOS DE CONTACTO
+  parentsPhone1 = new FormControl('', [Validators.required]);
+  parentsPhone2 = new FormControl('', [Validators.required]);
+
 
   getErrorMessage() {
+
+
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
-
     return this.email.hasError('email') ? 'Not a valid email' : '';
+
+
+    //MENSAJE ERROR APELLIDOS
+    if (this.lastNameParent.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.lastNameParent.hasError('lastNameParent') ? 'Espacio Vacio' : '';
+
+
+//MENSAJE ERROR NOMRBES
+    if (this.nameParent.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.nameParent.hasError('nameParent') ? 'Espacio Vacio' : '';
+
+
+    //MENSAJE ERROR CEDULA REPRESENTANTE
+    if (this.parentCI.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.parentCI.hasError('parentCI') ? 'Espacio Vacio' : '';
+
+
+
+//MENSAJE ERROR PARENTEZCO
+    if (this.familiar.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.familiar.hasError('familiar') ? 'Espacio Vacio' : '';
+
+
+
+//MENSAJE ERROR DIRECCION REPRESENTANTE
+    if (this.addressParent.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.addressParent.hasError('addressParent') ? 'Espacio Vacio' : '';
+
+
+    //MENSAJE ERROR CELULAR1
+    if (this.parentsPhone1.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.parentsPhone1.hasError('parentsPhone1') ? 'Espacio Vacio' : '';
+
+    //MENSAJE ERROR CELULAR2
+    if (this.parentsPhone2.hasError('required')) {
+      return 'No dejar campos vacios';
+    }
+    return this.parentsPhone2.hasError('parentsPhone2') ? 'Espacio Vacio' : '';
+
+
+
+
+
+
+
+
+
+
+
+
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   ngOnInit(): void {
   }
