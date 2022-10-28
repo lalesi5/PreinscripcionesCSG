b9129f2eaa8260688130346d648da990d5f340c4
JeremyTorres07 <jeremy.torres@epn.edu.ec>
2022-10-28 07:56:02 -0500

    Guardar datos de formulario Representantes

--- Preinscripciones/src/app/Sistema/Finalizar/finalizar.component.ts
+++ Preinscripciones/src/app/Sistema/Finalizar/finalizar.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
-
 import {Router} from "@angular/router";
-
-
 @Component({
   selector: 'app-finalizar',
   templateUrl: './finalizar.component.html',
   styleUrls: ['./finalizar.component.css']
 })
 export class FinalizarComponent {
-
-
   constructor(private router: Router) { }
   onSubmit (): void{
+    console.log('Funciona este boton')
     this.router.navigate(['login']);
-
   }
-
 }
